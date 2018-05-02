4e45e2d01d9edc1e0a9bd73338a3f7070a7ad9ea
Fábio Dias Soares <fabio.soares@blendmobi.com>
2018-05-02 15:02:12 -0300

    full local builder

--- public/js/new.js
+++ public/js/new.js
@@ -4,6 +4,5 @@ var editorOptions = {showEmbededSurveyTab: true};
 var editor = new SurveyEditor.SurveyEditor("editorElement", editorOptions);
 
 function save(){
-  var yourNewSurveyJSON = editor.text;
   console.log(editor, editor.text)  ;
 }
--- survey.js
+++ survey.js
@@ -12,21 +12,43 @@ const html = readFileSync(`${__appBase}/views/index.html`).toString('utf-8');
 const newHtml = readFileSync(`${__appBase}/views/new.html`).toString('utf-8');
 const newJS = readFileSync(`${__appBase}/node_modules/surveyjs-editor/surveyeditor.js`).toString('utf-8');
 const css = readFileSync(`${__appBase}/node_modules/surveyjs-editor/surveyeditor.css`).toString('utf-8');
-const knockout = readFileSync(`${__appBase}/node_modules/survey-knockout/survey.ko.js`).toString('utf-8');
+const surveyKO = readFileSync(`${__appBase}/node_modules/survey-knockout/survey.ko.min.js`).toString('utf-8');
+const ko = readFileSync(`${__appBase}/node_modules/knockout/build/output/knockout-latest.js`).toString('utf-8');
+const ace = readFileSync(`${__appBase}/node_modules/ace-builds/src-min-noconflict/ace.js`).toString('utf-8');
+const aceLang = readFileSync(`${__appBase}/node_modules/ace-builds/src-min-noconflict/ext-language_tools.js`).toString('utf-8');
+const bootstrap = readFileSync(`${__appBase}/node_modules/bootstrap/dist/css/bootstrap.min.css`).toString('utf-8');
+const bootstrapMap = readFileSync(`${__appBase}/node_modules/bootstrap/dist/css/bootstrap.min.css.map`).toString('utf-8');
 
 app.use(bodyParser.urlencoded({'extended': true}));
 app.use(bodyParser.json());
 app.use(express.static(`${__appBase}/public`));
 
+app.get('/js/ext-language_tools.js', (req, res) => {
+  res.send(aceLang).end();
+});
+app.get('/js/ace.min.js', (req, res) => {
+  res.send(ace).end();
+});
 app.get('/js/surveyeditor.js', (req, res) => {
   res.send(newJS).end();
 });
+app.get('/js/knockout.js', (req, res) => {
+  res.send(ko).end();
+});
 app.get('/js/survey-knockout.js', (req, res) => {
-  res.send(knockout).end();
+  res.send(surveyKO).end();
 });
 app.get('/css/surveyeditor.css', (req, res) => {
-  req.headers['Content-Type'] = 'text/css';
+  res.set('Content-Type', 'text/css');
   res.send(css).end();
+});
+app.get('/css/bootstrap.min.css', (req, res) => {
+  res.set('Content-Type', 'text/css');
+  res.send(bootstrap).end();
+});
+app.get('/css/bootstrap.min.css.map', (req, res) => {
+  res.set('Content-Type', 'text/css');
+  res.send(bootstrapMap).end();
 });
 app.get('/new', (req, res) => {
   res.send(newHtml).end();
